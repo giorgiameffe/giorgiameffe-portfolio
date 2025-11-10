2be1d37d2ebf999ae016498cd5b11ffdb8ef6460
giorgiameffe <giorgiameffe03@gmail.com>
2025-11-10 13:26:40 +0100

    feat: create sample data file for projects

---
+++ src/data/projects.ts
@@ -0,0 +1,36 @@
+// Interfaccia per i progetti, da implementare per la card del dettaglio
+
+interface Project {
+    slug: string;
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+}
+
+const projects: Project[] = [
+
+    {
+        slug: "Progetto uno",
+        title: "Titolo del progetto",
+        description: "Descrizione del progetto",
+        image: "Immagine del progetto",
+        link: "Link del progetto"
+    },
+    {
+        slug: "Progetto uno",
+        title: "Titolo del progetto",
+        description: "Descrizione del progetto",
+        image: "Immagine del progetto",
+        link: "Link del progetto"
+    },
+    {
+        slug: "Progetto uno",
+        title: "Titolo del progetto",
+        description: "Descrizione del progetto",
+        image: "Immagine del progetto",
+        link: "Link del progetto"
+    }
+]
+
+export default projects;
