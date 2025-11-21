2baeaa9f7af6a5d696da75f7544355da49aed86b
giorgiameffe <giorgiameffe03@gmail.com>
2025-11-21 13:22:54 +0100

    feat: add imgs for ProjectPage

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,15 +1,24 @@
 import projects from "../data/projects";
 import ProjectCard from "../components/ProjectCard";
+import { useContext } from "react";
+import GlobalContext from "../contexts/GlobalContext";
 
 const ProjectsPage = () => {
 
+    const { isDark } = useContext(GlobalContext);
+
     return (
-        <section className="min-h-screen pt-20 pb-20 px-4">
+
+        <section className="min-h-screen mt-6 md:mt-10 lg:mt-20 pb-20 px-4">
             <div className="max-w-6xl mx-auto">
                 {/* Titolo + intro */}
-                <div className="max-w-2xl mx-auto text-center mb-12">
-                    <h1 className="text-3xl font-bold mb-4">Progetti</h1>
-                    <p className="text-gray-700 dark:text-gray-300 text-lg leading-relaxed">
+                <div className="max-w-2xl mx-auto text-center mb-12 md:flex md:justify-center md:items-center">
+                    <img
+                        src={isDark ? "/img/giorgia-projects-dark.png" : "/img/giorgia-projects-light.png"}
+                        alt="Foto personale"
+                        className="w-55 md:w-65 lg:w-75 lg:mr-20 h-auto mx-auto"
+                    />
+                    <p className="text-gray-800 dark:text-gray-200 text-lg leading-relaxed">
                         Qui trovi una raccolta dei progetti che ho realizzato nel mio percorso...
                     </p>
                 </div>
