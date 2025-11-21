287b620e9e5002be87896bf053be204598280ef4
giorgiameffe <giorgiameffe03@gmail.com>
2025-11-21 15:40:20 +0100

    feat: add bg radial-gradient

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -14,13 +14,13 @@ const ProjectsPage = () => {
             <div className="max-w-6xl mx-auto">
 
                 {/* ---------------- Titolo + Intro ---------------- */}
-                <div className="max-w-2xl mx-auto text-center mb-12 md:flex md:justify-center md:items-center">
+                <div className="max-w-2xl lg:max-w-4xl mx-auto text-center mb-12 md:flex md:justify-center md:items-center">
 
                     {/* Immagine animata con Framer Motion */}
                     <motion.img
                         src={isDark ? "/img/giorgia-projects-dark.png" : "/img/giorgia-projects-light.png"} // immagine light/dark
                         alt="Foto personale"
-                        className="w-55 md:w-65 lg:w-75 lg:mr-20 h-auto mx-auto"
+                        className="w-55 md:w-65 lg:w-75 lg:mr-20 h-auto mx-auto md:mr-6"
                         initial={{ opacity: 0, x: -50 }} // Stato iniziale: trasparente e spostata 50px a sinistra
                         animate={{ opacity: 1, x: 0 }} // Stato finale: visibile e posizione originale
                         transition={{ duration: 0.8, ease: "easeOut" }} // Durata 0.8s, easing naturale
@@ -28,14 +28,22 @@ const ProjectsPage = () => {
 
                     {/* Testo animato con Framer Motion */}
                     <motion.p
-                        className="text-gray-800 dark:text-gray-200 text-lg leading-relaxed"
+                        className="text-gray-800 dark:text-gray-200 text-lg leading-relaxed relative 
+                                     pt-4 pb-6 px-4 mb-2 md:mb-0
+                                     [background-image:radial-gradient(ellipse_at_center,_rgba(179,224,255,0.75)_0%,_transparent_75%)]
+                                     dark:[background-image:radial-gradient(ellipse_at_center,_rgba(5,2,49,0.95)_0%,_transparent_60%)]"
                         initial={{ opacity: 0, x: 50 }} // Inizio trasparente e spostato 50px a destra
                         animate={{ opacity: 1, x: 0 }} // Finale visibile e posizione normale
                         transition={{ duration: 0.8, ease: "easeOut", delay: 0.3 }} // Delay 0.3s rispetto all'immagine
                     >
-                        Qui trovi una raccolta dei progetti che ho realizzato nel mio percorso...
+                        Dove il codice diventa <strong>interfaccia</strong> e il design diventa <strong>esperienza</strong>: esplora i progetti in cui semplicità
+                        e cura dei dettagli incontrano la <strong>passione</strong> per la UX/UI.
                     </motion.p>
                 </div>
+
+                <h2 className="flex items-center justify-center gap-2 text-2xl md:text-3xl font-bold text-gray-900 dark:text-gray-200 mb-6 mt-4">
+                    I miei progetti
+                </h2>
 
                 {/* ---------------- Griglia dei progetti ---------------- */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 place-items-center gap-8">
@@ -45,7 +53,7 @@ const ProjectsPage = () => {
                     ))}
                 </div>
             </div>
-        </section>
+        </section >
     );
 };
 
