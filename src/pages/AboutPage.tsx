dd2115a3728b652a48a98254403793dbcf4a62d8
giorgiameffe <giorgiameffe03@gmail.com>
2025-12-05 11:18:36 +0100

    feat: add framer-motion to AboutPage h1

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import NarrativeBlockList from "../components/NarrativeBlockList";
 import SkillsSection from "../components/SkillsSection";
+import { motion } from "framer-motion";
 
 const AboutPage: React.FC = () => {
 
     return (
 
         <div className="mt-10 lg:mt-20">
-            <h1 className="font-extrabold text-2xl md:text-3xl lg:text-4xl text-center">Dall’estetica al codice: la mia storia</h1>
+
+            {/* Titolo animato con Framer Motion*/}
+            <motion.h1
+                className="font-extrabold text-2xl md:text-3xl lg:text-4xl text-center"
+                initial={{ opacity: 0, y: -30 }} // Inizio un po' più in alto
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6, ease: "easeOut" }}
+            >
+                Dall’estetica al codice: la mia storia
+            </motion.h1>
             <NarrativeBlockList />
             <SkillsSection />
         </div>
