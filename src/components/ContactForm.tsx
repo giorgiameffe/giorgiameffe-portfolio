74f8b73b39e63c41226e40b0fb0c4aae2f7cd05a
giorgiameffe <giorgiameffe03@gmail.com>
2025-12-09 15:18:32 +0100

    feat: add lastName input and style

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 import type { FormEvent, ChangeEvent } from "react";
 
 interface FormData {
-    firstname: string;
-    lastname: string;
+    firstName: string;
+    lastName: string;
     email: string;
     subject: string;
     message: string;
@@ -13,8 +13,8 @@ interface FormData {
 const ContactForm: React.FC = () => {
 
     const [formData, setFormData] = useState<FormData>({
-        firstname: "",
-        lastname: "",
+        firstName: "",
+        lastName: "",
         email: "",
         subject: "",
         message: ""
@@ -35,17 +35,45 @@ const ContactForm: React.FC = () => {
     return (
 
         // Form contatti
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="md:max-w-2xl lg:max-w-4xl mx-auto">
 
-            {/* Input per il nome */}
-            <input
-                type="text"
-                name="name"
-                placeholder="Nome"
-                value={formData.firstname}
-                onChange={handleChange}
-                required
-            />
+            {/* Container Nome e Cognome */}
+            <div className="flex flex-col md:flex-row md:gap-6 lg:gap-4">
+                {/* Nome */}
+                <div className="flex-1 mb-4 lg:mb-0">
+                    <div className="flex items-center mb-1">
+                        <label htmlFor="firstName" className="font-medium mr-2">Nome:</label>
+                    </div>
+                    <input
+                        type="text"
+                        id="firstName"
+                        name="firstName"
+                        placeholder="Inserisci il tuo nome"
+                        value={formData.firstName}
+                        onChange={handleChange}
+                        required
+                        className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                </div>
+
+                {/* Cognome */}
+                <div className="flex-1">
+                    <div className="flex items-center mb-1">
+                        <label htmlFor="lastName" className="font-medium mr-2">Cognome:</label>
+                    </div>
+                    <input
+                        type="text"
+                        id="lastName"
+                        name="lastName"
+                        placeholder="Inserisci il tuo cognome"
+                        value={formData.lastName}
+                        onChange={handleChange}
+                        required
+                        className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                </div>
+            </div>
+
         </form>
     )
 
