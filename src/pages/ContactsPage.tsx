baa64633b1a7758507e0534a0d78eadc4f54ff25
giorgiameffe <giorgiameffe03@gmail.com>
2025-12-09 12:15:34 +0100

    feat: refactor ContactsPage structure and add tag p and animation

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -4,20 +4,41 @@ import { motion } from "framer-motion";
 const ContactsPage: React.FC = () => {
 
     return (
-        <div className="mt-10 lg:mt-20">
 
-            {/* Titolo animato con Framer Motion*/}
-            <motion.h1
-                className="font-extrabold text-2xl md:text-3xl lg:text-4xl text-center"
-                initial={{ opacity: 0, y: -30 }} // Inizio un po' più in alto
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, ease: "easeOut" }}
-            >
-                Costruiamo qualcosa insieme
-            </motion.h1>
+        <section className="min-h-screen mt-12 md:mt-16 lg:mt-20 mb-20 px-4">
 
-        </div>
-    )
-}
+            {/* Contenitore principale */}
+            <div className="max-w-6xl mx-auto">
+
+                {/* Titolo con animazione identica ai progetti */}
+                <motion.h1
+                    className="text-2xl md:text-3xl lg:text-4xl font-extrabold text-gray-900 dark:text-gray-200 mb-6 text-center"
+                    initial={{ opacity: 0, y: -30 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1, ease: "easeOut" }}
+                >
+                    Costruiamo qualcosa insieme
+                </motion.h1>
+
+                {/* Sottotitolo animato con stessa logica del ProjectsPage */}
+                <div className="max-w-2xl md:max-w-xl mx-auto text-center mb-12 md:flex md:justify-center md:items-center">
+                    <motion.p
+                        className="text-gray-800 dark:text-gray-200 text-lg leading-relaxed relative 
+                       pt-4 pb-6 px-4 mb-2 md:mb-0"
+                        initial={{ opacity: 0, x: 50 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.8, ease: "easeOut", delay: 0.3 }}
+                    >
+                        Hai un’idea, un progetto o semplicemente una domanda?
+                        Scrivimi tramite il form o contattami sui miei social.
+                        <br />
+                        <strong> Sarò felice di ascoltarti.</strong>
+                    </motion.p>
+                </div>
+
+            </div>
+        </section>
+    );
+};
 
 export default ContactsPage;
