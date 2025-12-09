18fbb744c41df5dc634a66e65bdab3347a919d67
giorgiameffe <giorgiameffe03@gmail.com>
2025-12-09 11:50:29 +0100

    feat: add h1 and animation using framer motion

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const ContactsPage: React.FC = () => {
 
     return (
-        <div>
-            <h1 className="font-bold">Sono nella pagina Contatti</h1>
+        <div className="mt-10 lg:mt-20">
+
+            {/* Titolo animato con Framer Motion*/}
+            <motion.h1
+                className="font-extrabold text-2xl md:text-3xl lg:text-4xl text-center"
+                initial={{ opacity: 0, y: -30 }} // Inizio un po' più in alto
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, ease: "easeOut" }}
+            >
+                Costruiamo qualcosa insieme
+            </motion.h1>
+
         </div>
     )
 }
