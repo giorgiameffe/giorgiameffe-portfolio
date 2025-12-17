74b4386da05fc42ad31b0f501e2a88fd99182810
giorgiameffe <giorgiameffe03@gmail.com>
2025-12-17 23:41:53 +0100

    feat: create projects folder for projects images

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -20,21 +20,21 @@ const projects: Project[] = [
         slug: "header-ui-custom",
         title: "Header UI custom",
         description: "Progetto individuale sulla creazione di un componente Header UI custom per un sito di cosmetici naturali.",
-        image: "/img/header-preview.png",
+        image: "/images/projects/header-preview.png",
         featured: true
     },
     {
         slug: "bool-shop",
         title: "BoolShop",
         description: "Progetto Full Stack di gruppo sulla creazione di un e-commerce di giochi da tavolo",
-        image: "/img/boolshop-preview.png",
+        image: "/images/projects/boolshop-preview.png",
         featured: true
     },
     {
         slug: "cine-circle",
         title: "Cine Circle",
         description: "Progetto Full Stack individuale sulla creazione e gestione di un blog di film.",
-        image: "/img/cine-circle-preview.png",
+        image: "/images/projects/cine-circle-preview.png",
         featured: true
     },
 ]
