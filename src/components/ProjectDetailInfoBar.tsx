a2c31b6e7199874b0a939340bbb6e9538de2d101
giorgiameffe <giorgiameffe03@gmail.com>
2025-12-19 12:25:20 +0100

    feat: create ProjectDetailInfoBar component and import it

---
+++ src/components/ProjectDetailInfoBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectDetailInfoBar: React.FC = () => {
+
+    return (
+
+        <div>Sono info bar della pagina dettaglio</div>
+    )
+}
+
+export default ProjectDetailInfoBar;
--- src/pages/ProjectDetailPage.tsx
+++ src/pages/ProjectDetailPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import ProjectsDetailHero from "../components/ProjectDetailHero";
+import ProjectDetailHero from "../components/ProjectDetailHero";
+import ProjectDetailInfoBar from "../components/ProjectDetailInfoBar";
 
 const ProjectDetailsPage: React.FC = () => {
 
@@ -7,7 +8,8 @@ const ProjectDetailsPage: React.FC = () => {
 
         <section className="mt-12 md:mt-16 lg:mt-20 max-w-6xl mx-auto">
             <h1 className="font-bold">Sono nella pagina Dettagli Progetto</h1>
-            <ProjectsDetailHero />
+            <ProjectDetailHero />
+            <ProjectDetailInfoBar />
         </section>
     )
 }
