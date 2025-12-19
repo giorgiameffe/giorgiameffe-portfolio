2f3d560ff5ffd1b2e32c755caa514be355d6baaf
giorgiameffe <giorgiameffe03@gmail.com>
2025-12-19 13:22:02 +0100

    feat: add slug and ProjectDetailHero data

--- src/components/ProjectDetailHero.tsx
+++ src/components/ProjectDetailHero.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 
-const ProjectDetailHero: React.FC = () => {
-
-    return (
-
-        <div>Sono l'hero della pagina dettaglio del progetto</div>
-    )
+interface ProjectDetailHeroProps {
+    title: string;
+    briefDescription: string;
+    heroImage: string;
 }
+
+const ProjectDetailHero: React.FC<ProjectDetailHeroProps> = ({
+    title,
+    briefDescription,
+    heroImage,
+}) => {
+    return (
+        <>
+            <h1>{title}</h1>
+            <p>{briefDescription}</p>
+            <img src={heroImage} alt={title} />
+        </>
+    );
+};
 
 export default ProjectDetailHero;
--- src/pages/ProjectDetailPage.tsx
+++ src/pages/ProjectDetailPage.tsx
@@ -3,19 +3,36 @@ import ProjectDetailHero from "../components/ProjectDetailHero";
 import ProjectDetailInfoBar from "../components/ProjectDetailInfoBar";
 import ProjectDetailContent from "../components/ProjectDetailContent";
 import ProjectDetailGallery from "../components/ProjectDetailGallery";
+import { useParams } from "react-router-dom";
+import ProjectsDetail from "../data/projectsDetailData";
+
 
 const ProjectDetailsPage: React.FC = () => {
 
-    return (
+    const { slug } = useParams<{ slug: string }>();
 
+    const project = ProjectsDetail.find(project => project.slug === slug);
+
+    if (!project) {
+
+        return <p>Progetto non trovato</p>;
+    }
+
+    return (
         <section className="mt-12 md:mt-16 lg:mt-20 max-w-6xl mx-auto">
-            <h1 className="font-bold">Sono nella pagina Dettagli Progetto</h1>
-            <ProjectDetailHero />
+
+            {/* Hero della pagina di dettaglio */}
+            <ProjectDetailHero
+                title={project.title}
+                briefDescription={project.briefDescription}
+                heroImage={project.heroImage}
+            />
+
             <ProjectDetailInfoBar />
             <ProjectDetailContent />
             <ProjectDetailGallery />
         </section>
-    )
-}
+    );
+};
 
 export default ProjectDetailsPage;
