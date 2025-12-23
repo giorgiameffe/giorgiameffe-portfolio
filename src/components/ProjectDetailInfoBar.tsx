b4e6eb6480e77f8fa6cd0d4a226c6a40b01864b6
giorgiameffe <giorgiameffe03@gmail.com>
2025-12-23 12:22:52 +0100

    feat: fix tablet responsivity

--- src/components/ProjectDetailInfoBar.tsx
+++ src/components/ProjectDetailInfoBar.tsx
@@ -31,7 +31,7 @@ const ProjectDetailInfoBar: React.FC<InfoBarProps> = ({
 
     return (
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mb-8">
 
             {/* Sezione dettagli del progetto */}
             <section>
