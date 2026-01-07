d0a2c1fcd73f8df7898831017ff441cbf6252608
giorgiameffe <giorgiameffe03@gmail.com>
2026-01-07 22:41:22 +0100

    feat: add categories property to projectsData

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -8,6 +8,10 @@ export interface Project {
     description: string;
     image: string;
     featured?: boolean;
+    categories?: {
+        name: "UI Design System" | "Front-end" | "Back-end";
+        color: string;
+    }[];
 }
 
 // ==========================================================
@@ -21,21 +25,47 @@ const projects: Project[] = [
         title: "Header UI custom",
         description: "Progetto individuale sulla creazione di un componente Header UI custom per un sito di cosmetici naturali.",
         image: "/images/projects/header-ui-project/header-preview.png",
-        featured: true
+        featured: true,
+        categories: [
+            {
+                name: "UI Design System",
+                color: "bg-purple-200 text-purple-800 dark:bg-purple-950 dark:text-purple-200"
+            }
+        ]
     },
     {
         slug: "bool-shop",
         title: "BoolShop",
         description: "Progetto Full Stack di gruppo sulla creazione di un e-commerce di giochi da tavolo",
         image: "/images/projects/boolshop-project/boolshop-preview.png",
-        featured: true
+        featured: true,
+        categories: [
+            {
+                name: "Front-end",
+                color: "bg-indigo-200 text-indigo-950 dark:bg-indigo-950 dark:text-indigo-200"
+            },
+            {
+                name: "Back-end",
+                color: "bg-green-200 text-green-950 dark:bg-green-950 dark:text-green-200"
+            }
+        ]
     },
     {
         slug: "cine-circle",
         title: "Cine Circle",
         description: "Progetto Full Stack individuale sulla creazione e gestione di un blog di film.",
         image: "/images/projects/cine-circle-project/cine-circle-preview.png",
-        featured: true
+        featured: true,
+        categories: [
+            {
+                name: "Front-end",
+                color: "bg-indigo-200 text-indigo-950 dark:bg-indigo-950 dark:text-indigo-200"
+            },
+            {
+                name: "Back-end",
+                color: "bg-green-200 text-green-950 dark:bg-green-950 dark:text-green-200"
+            }
+        ]
     },
 ]
 
