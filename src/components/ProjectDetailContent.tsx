a0a78035be381bd8a160f581d96e680b91f875bd
giorgiameffe <giorgiameffe03@gmail.com>
2026-01-12 16:45:34 +0100

    feat: fix mobile responsive

--- src/components/ProjectDetailContent.tsx
+++ src/components/ProjectDetailContent.tsx
@@ -20,7 +20,7 @@ const ProjectDetailContent: React.FC<ContentProps> = ({ description }) => {
                 <h2 className="text-2xl font-bold mb-4 text-center">
                     Obiettivo e scopo
                 </h2>
-                <p className="text-lg md:text-xl text-gray-800 dark:text-gray-200 leading-relaxed font-medium">
+                <p className="text-lg md:text-xl text-center md:text-start text-gray-800 dark:text-gray-200 leading-relaxed font-medium">
                     {description.scopeAndGoal}
                 </p>
             </div>
--- src/components/ProjectDetailHero.tsx
+++ src/components/ProjectDetailHero.tsx
@@ -17,7 +17,7 @@ const ProjectDetailHero: React.FC<ProjectDetailHeroProps> = ({
 
         <div className="flex flex-col items-center text-center">
             <motion.h1
-                className="text-2xl md:text-3xl lg:text-4xl font-extrabold text-gray-900 dark:text-gray-200 mb-6"
+                className="text-2xl md:text-3xl lg:text-4xl font-extrabold text-gray-900 dark:text-gray-200 mb-4 md:mb-6"
                 initial={{ opacity: 0, y: -30 }} // Inizio un po' più in alto
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1, ease: "easeOut" }}
@@ -26,7 +26,7 @@ const ProjectDetailHero: React.FC<ProjectDetailHeroProps> = ({
             </motion.h1>
 
             <motion.p
-                className="text-gray-800 dark:text-gray-200 text-lg leading-relaxed mb-2 md:mb-12 max-w-3xl"
+                className="text-gray-800 dark:text-gray-200 text-md md:text-lg leading-relaxed mb-6 md:mb-8 max-w-3xl"
                 initial={{ opacity: 0, x: 50 }} // Inizio trasparente e spostato 50px a destra
                 animate={{ opacity: 1, x: 0 }} // Finale visibile e posizione normale
                 transition={{ duration: 0.8, ease: "easeOut", delay: 0.3 }} // Delay 0.3s
