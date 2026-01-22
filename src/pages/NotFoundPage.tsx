dc4269ed8459df80c42033b57e9302685beedeb4
giorgiameffe <giorgiameffe03@gmail.com>
2026-01-22 22:23:11 +0100

    feat: add margot image for 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,11 +1,18 @@
-import React from "react"
+import React from "react";
+import margotImage from "../assets/margot-404.png";
 
 const NotFoundPage: React.FC = () => {
 
     return (
 
-        <section className="flex justify-center items-center h-100">
-            <h1 >Not Found Page</h1>
+        <section className="flex justify-center items-center min-h-screen flex-col">
+
+            <img
+                src={margotImage}
+                alt="Immagine di un gatto per la pagina 404"
+                className="w-80"
+            />
+
         </section>
     )
 }
